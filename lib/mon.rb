b812c3a421120bfd98a10afae7d1963ac2d1cce8
Tyler Benziger <tabenziger@gmail.com>
2014-08-01 00:09:13 -0700

    Clean up

--- lib/mon.rb
+++
@@ -1,9 +0,0 @@
-require "mon/battle"
-require "mon/move"
-require "mon/pokedex"
-require "mon/pokemon"
-require "mon/stats"
-require "mon/version"
-
-module Mon
-end
--- lib/mon/version.rb
+++
@@ -1,3 +0,0 @@
-module Mon
-  VERSION = "0.0.2"
-end
