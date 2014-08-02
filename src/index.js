55b72eb52a4c87c130d5c0736d62bd51f532a984
Tyler Benziger <tabenziger@gmail.com>
2014-08-02 13:50:13 -0700

    First pass at conversion

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var battle = require( './battle' );
+var move = require( './move' );
+var pokedex = require( './pokedex' );
+var stats = require( './stats' );
+var version = require( './version' );
+
+module.exports = {};
---
+++ test.js
@@ -0,0 +1,16 @@
+var Battle = require( './src/battle' );
+var _ = require( 'lodash-node' );
+
+var battle = new Battle();
+battle.chooseHome( 'Charizard' );
+
+while ( battle.battling() ) {
+  var move = _.shuffle( battle.home.moves() )[ 0 ];
+  var moves = battle.useHome( move.name );
+  _.each( moves, function( pokemon ) {
+    console.log( '> ' + pokemon.name + ' used ' + pokemon.lastMove.name );
+  });
+
+  console.log( battle.home.name, battle.home.hp );
+  console.log( battle.visitor.name, battle.visitor.hp );
+}
